5b929542399f086dcd3f19a30dba0b8b2919ef03
Seano <sean01234@yahoo.com>
2023-09-01 21:20:43 -0400

    stuck on the math route

--- server/app.py
+++ server/app.py
@@ -24,17 +24,57 @@ def mycount(parameter):
 
 
 
-#@app.route('/count/<parameter>')
-#def mycount(parameter):
-#    result_string = "\n".join(map(str, parameter))
-#    for item in parameter:
-#        print(item)
-#    return result_string
+@app.route('/math/<parameters>/')
+def mymath(parameters):
+    # Split the parameters using '/' as the separator
+    params = parameters.split('/')
+    
+    # Check if there are exactly three parameters (num1, operation, num2)
+    if len(params) != 3:
+        return "Invalid URL format. Use /math/num1/operation/num2"
+    
+    num1, operation, num2 = params
+    
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return "Invalid number format. Use valid numbers."
+    
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Division by zero is not allowed."
+    elif operation == '%':
+        if num2 != 0:
+            result = num1 % num2
+        else:
+            return "Modulo by zero is not allowed."
+    else:
+        return "Invalid operation. Supported operations are +, -, *, div, and %."
+    
+    return f"Result: {result}"
 
-#my_range = range(param)
-#result = mycount(my_range)
-##print("Resulting string:")
-#print(result)
+
+# cant figure out this error
+#    def test_math_route(self):
+#        '''has a resource available at "/math/<parameters>".'''
+#        response = app.test_client().get('/math/5/+/5')
+#>       assert(response.status_code == 200)
+#E       assert 404 == 200
+#E        +  where 404 = <WrapperTestResponse streamed [404 NOT FOUND]>.status_code
+
+#testing/app_test.py:51: AssertionError
+#============================ short test summary info ==================================
+#FAILED ../Flask application in flask_app.py has a resource available at "/math/<parameters>". - assert 404 == 200
+#
 
 
 
